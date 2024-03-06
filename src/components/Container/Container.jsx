3d61e343e3e74680f40eda6029ce286ab1225c61
Daria <dargrab07@gmail.com>
2024-03-06 14:43:03 +0100

    fixed display of navbar and added background to home section

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,9 +1,16 @@
 import clsx from "clsx";
+import styles from "./Container.module.scss";
 
 const Container = () => {
   return (
-    <section className={clsx("container")}>
-      <h1>I design and create web applications. Simple, yet elegant</h1>
+    <section>
+      <div className={styles.homeBackground}>
+        <div className={clsx("container")}>
+          <h1 className={styles.homeTitle}>
+            I design and create web applications. Simple, yet elegant
+          </h1>
+        </div>
+      </div>
     </section>
   );
 };
