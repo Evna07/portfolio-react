a1c8b98b17190f7be0ab17bc0acbb51cc679b3ac
Daria <dargrab07@gmail.com>
2024-03-11 17:00:05 +0100

    modified navbar

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,9 +1,12 @@
+import clsx from "clsx";
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <footer id="contact" className={styles.footer}>
-      <h4>Daria Paulina Grabowska - all rights reserved, 2024</h4>
+      <div className={clsx(".container", styles.footerContainer)}>
+        <h4>Daria Paulina Grabowska - all rights reserved, 2024</h4>
+      </div>
     </footer>
   );
 };
