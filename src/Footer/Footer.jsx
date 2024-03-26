a0e226877ddee9d627811cb212e842e653fa5269
Daria <dargrab07@gmail.com>
2024-03-26 12:15:46 +0100

    added icons to footer and fixed responsive display

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,11 +1,20 @@
-import clsx from "clsx";
 import styles from "./Footer.module.scss";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer id="contact" className={styles.footer}>
-      <div className={clsx(".container", styles.footerContainer)}>
+      <div className={styles.footerContainer}>
         <h4>Daria Paulina Grabowska - all rights reserved, 2024</h4>
+
+        <div className={styles.footerLinksContainer}>
+          <a className={styles.footerLink} href="google.com">
+            <BsLinkedin />
+          </a>
+          <a className={styles.footerLink} href="google.com">
+            <BsGithub />
+          </a>
+        </div>
       </div>
     </footer>
   );
--- src/components/Projects/Project/ProjectItem.jsx
+++ src/components/Projects/Project/ProjectItem.jsx
@@ -6,33 +6,13 @@ const ProjectItem = ({ item }) => {
     <li key={nanoid()}>
       <div className={styles.projectItem}>
         {item.isNew && <div className={styles.newProjectTag}></div>}
-        <h2>{item.name}</h2>
+        <a href={item.url} target="blank">
+          <h2>{item.name}</h2>
+        </a>
         {item.isNew && <p>(new)</p>}
       </div>
-      {/* TODO: after hovering over item make it bigger and display iframe */}
-      {/* <div>
-        <iframe
-          src="about:blank"
-          frameborder="0"
-          className={styles.projectItem}
-        ></iframe>
-      </div> */}
     </li>
   );
 };
 
 export default ProjectItem;
-
-// {
-//   /* <li key={nanoid()}>
-//       <iframe
-//         src={"about:blank"}
-//         frameborder="0"
-//         title={item.name}
-//         className={styles.projectItem}
-//       >
-//         {item.isNew && <div className={styles.newProjectTag}></div>}
-//         {item.isNew && <p>(new)</p>}
-//       </iframe>
-//     </li> */
-// }
